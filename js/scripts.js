32f570f54a004958a112b227ef4516c3904f93d5
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-08-28 01:49:33 -0700

    create handler function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 function handleSuggestion(event) {
   event.preventDefault();
+  const question1 = document.querySelector("input[name='question1']:checked").value;
+  const question2 = document.querySelector("input[name='question2']:checked").value;
+  const question3 = document.querySelector("input[name='question3']:checked").value;
+  const question4 = document.querySelector("input[name='question4']:checked").value;
+  const question5 = document.querySelector("input[name='question5']:checked").value;
 }
+
