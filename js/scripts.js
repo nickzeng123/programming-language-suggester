ad20d949eb84d8998137971ca0f760f1d1b4fcd9
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-08-29 17:05:43 -0700

    add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,24 @@ window.addEventListener("load", function() {
   let form = document.querySelector("form");
   let suggestion = document.querySelector("div#suggestion");
   
-  const question1 = document.querySelector("input[name='question1']:checked").value;
-  const question2 = document.querySelector("input[name='question2']:checked").value;
-  const question3 = document.querySelector("input[name='question3']:checked").value;
-  const question4 = document.querySelector("input[name='question4']:checked").value;
-  const question5 = document.querySelector("input[name='question5']:checked").value;
-  event.preventDefault();
+  //const question1 = document.querySelector("input[name='question1']:checked").value;
+  //const question2 = document.querySelector("input[name='question2']:checked").value;
+  //const question3 = document.querySelector("input[name='question3']:checked").value;
+  //const question4 = document.querySelector("input[name='question4']:checked").value;
+  //const question5 = document.querySelector("input[name='question5']:checked").value;
+
+  form.addEventListener("submit", function(event) {
+    const question1 = document.querySelector("input[name='question1']:checked").value;
+    const question2 = document.querySelector("input[name='question2']:checked").value;
+    const question3 = document.querySelector("input[name='question3']:checked").value;
+    const question4 = document.querySelector("input[name='question4']:checked").value;
+    const question5 = document.querySelector("input[name='question5']:checked").value;
+
+    form.removeAttribute("class");
+    event.preventDefault();
+  })
+
+  
+  
 }
 
