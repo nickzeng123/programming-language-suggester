044e72f18ccc0294c9450197bece4eb7310575b0
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-08-29 17:16:10 -0700

    create hidden class

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,20 @@
 
 window.addEventListener("load", function() {
-  let form = document.querySelector("form");
-  let suggestion = document.querySelector("div#suggestion");
-  
-  //const question1 = document.querySelector("input[name='question1']:checked").value;
-  //const question2 = document.querySelector("input[name='question2']:checked").value;
-  //const question3 = document.querySelector("input[name='question3']:checked").value;
-  //const question4 = document.querySelector("input[name='question4']:checked").value;
-  //const question5 = document.querySelector("input[name='question5']:checked").value;
+  const form = document.querySelector("form");
+  const suggestion = document.querySelector("div#suggestion");
 
   form.addEventListener("submit", function(event) {
+    event.preventDefault();
     const question1 = document.querySelector("input[name='question1']:checked").value;
     const question2 = document.querySelector("input[name='question2']:checked").value;
     const question3 = document.querySelector("input[name='question3']:checked").value;
     const question4 = document.querySelector("input[name='question4']:checked").value;
     const question5 = document.querySelector("input[name='question5']:checked").value;
 
-    form.removeAttribute("class");
-    event.preventDefault();
+
+    form.removeAttribute("class");  
+    document.getElementById("output").innerText = result;
   })
 
-  
-  
-}
+})
 
