5af142d26189caf8e4e28e7d010059dfca309cac
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-08-29 17:05:10 -0700

    add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-function handleSuggestion(event) {
-  event.preventDefault();
+
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  let suggestion = document.querySelector("div#suggestion");
+  
   const question1 = document.querySelector("input[name='question1']:checked").value;
   const question2 = document.querySelector("input[name='question2']:checked").value;
   const question3 = document.querySelector("input[name='question3']:checked").value;
   const question4 = document.querySelector("input[name='question4']:checked").value;
   const question5 = document.querySelector("input[name='question5']:checked").value;
+  event.preventDefault();
 }
 
