f214d070ecc70aec50ad52cad1626ce93552594c
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-09-02 01:51:32 -0700

    fix result branching

--- js/scripts.js
+++ js/scripts.js
@@ -11,28 +11,29 @@ window.addEventListener("load", function() {
     const questionInput5 = document.querySelectorAll("input[name='question5']:checked").value;
     
     let score;
-    questionInputsArray.forEach(function(element) {
-      if (questionInput1 === "yes" || questionInput3 === "yes") {
-        score = 15;} else if (questionInput2 === "yes" || questionInput4 === "yes") {
-          score = 25;
-        } else if (questionInput5 === "yes") {
-          score = 35;
-        }
-    })
-
+    if (questionInput1 === "yes" || questionInput3 === "yes") {
+      score = 15;
+    } else if (questionInput2 === "yes" || questionInput4 === "yes") {
+      score = 25;
+    } else if (questionInput5 === "yes") {
+      score = 35;
+    }
+  
     let result;
-    if (score >= 40) {
-      result = "Javascript";
-    } else if (score >= 20) {
+    if (score === 15) {
       result = "Python";
+    } else if (score === 25) {
+      result = "Javascript";
+    } else if (score === 35) {
+      result = "Swift";
     } else {
       result = "HTML";
     }
 
-    document.querySelector("span#output").innerText = "html";
+    document.querySelector("span#output").innerText = result;
     suggesion.removeAttribute("class");  
     event.preventDefault();
   });
 
-// });
+});
 
