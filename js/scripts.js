5bb67ec92fa2768bd718d79ced5556e9d84b23e8
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-09-02 01:46:04 -0700

    revise questionInput branching

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,20 @@ window.addEventListener("load", function() {
   let suggestion = document.querySelector("div#suggestion");
 
   form.addEventListener("submit", function(event) {   
-    const questionInputs = document.querySelectorAll("input[name='question']:checked").value;
-    const questionInputsArray = Array.from(questionInputs);
-
+    const questionInput1 = document.querySelectorAll("input[name='question1']:checked").value;
+    const questionInput2 = document.querySelectorAll("input[name='question2']:checked").value;
+    const questionInput3 = document.querySelectorAll("input[name='question3']:checked").value;
+    const questionInput4 = document.querySelectorAll("input[name='question4']:checked").value;
+    const questionInput5 = document.querySelectorAll("input[name='question5']:checked").value;
+    
     let score;
     questionInputsArray.forEach(function(element) {
-      if (element === "yes") {
-        score = score + 10;
-      };
+      if (questionInput1 === "yes" || questionInput3 === "yes") {
+        score = 15;} else if (questionInput2 === "yes" || questionInput4 === "yes") {
+          score = 25;
+        } else if (questionInput5 === "yes") {
+          score = 35;
+        }
     })
 
     let result;
@@ -23,10 +29,10 @@ window.addEventListener("load", function() {
       result = "HTML";
     }
 
-    document.querySelector("span#output").innerText = result;
-    form.removeAttribute("class");  
+    document.querySelector("span#output").innerText = "html";
+    suggesion.removeAttribute("class");  
     event.preventDefault();
   });
 
-});
+// });
 
