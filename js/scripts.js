8517baa3c5b112ed1624211a25073dff28941749
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-09-02 02:03:23 -0700

    create callback funtion

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,40 @@
-
-window.addEventListener("load", function() {
-  let form = document.querySelector("form");
-  let suggestion = document.querySelector("div#suggestion");
-
-  form.addEventListener("submit", function(event) {   
-    const questionInput1 = document.querySelectorAll("input[name='question1']:checked").value;
-    const questionInput2 = document.querySelectorAll("input[name='question2']:checked").value;
-    const questionInput3 = document.querySelectorAll("input[name='question3']:checked").value;
-    const questionInput4 = document.querySelectorAll("input[name='question4']:checked").value;
-    const questionInput5 = document.querySelectorAll("input[name='question5']:checked").value;
-    
-    let score;
-    if (questionInput1 === "yes" || questionInput3 === "yes") {
-      score = 15;
-    } else if (questionInput2 === "yes" || questionInput4 === "yes") {
-      score = 25;
-    } else if (questionInput5 === "yes") {
-      score = 35;
-    }
+function handleSurvey(event) {
+  event.preventDefault();
+  const questionInput1 = document.querySelectorAll("input[name='question1']:checked").value;
+  const questionInput2 = document.querySelectorAll("input[name='question2']:checked").value;
+  const questionInput3 = document.querySelectorAll("input[name='question3']:checked").value;
+  const questionInput4 = document.querySelectorAll("input[name='question4']:checked").value;
+  const questionInput5 = document.querySelectorAll("input[name='question5']:checked").value;
   
-    let result;
-    if (score === 15) {
+  let score;
+    if (questionInput1 === "yes" || questionInput3 === "yes") {
+      score = 1;
+    } else if (questionInput2 === "yes" || questionInput4 === "yes") {
+      score = 2;
+    } else if (questionInput5 === "yes") {
+      score = 3;
+    }
+
+  let result;
+    if (score === 1) {
       result = "Python";
-    } else if (score === 25) {
+    } else if (score === 2) {
       result = "Javascript";
-    } else if (score === 35) {
+    } else if (score === 3) {
       result = "Swift";
     } else {
       result = "HTML";
     }
+}
 
-    document.querySelector("span#output").innerText = result;
-    suggesion.removeAttribute("class");  
-    event.preventDefault();
-  });
+window.addEventListener("load", function() {
+  let form = document.querySelector("form#survey");
+  let suggestion = document.querySelector("div#suggestion");
 
+  form.addEventListener("submit", handleSurvey);
+    
+  document.querySelector("span#output").innerText = result;
+  suggesion.removeAttribute("class");    
 });
+
 
