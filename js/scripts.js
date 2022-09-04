d5ccee9c64e9e3360588325fcea680d35dfc4e99
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-09-03 23:19:14 -0700

    fix callback

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-function handleSurvey(event) {
+function handleSuggestion(event) {
   event.preventDefault();
-  const questionInput1 = document.querySelectorAll("input[name='question1']:checked").value;
-  const questionInput2 = document.querySelectorAll("input[name='question2']:checked").value;
-  const questionInput3 = document.querySelectorAll("input[name='question3']:checked").value;
-  const questionInput4 = document.querySelectorAll("input[name='question4']:checked").value;
-  const questionInput5 = document.querySelectorAll("input[name='question5']:checked").value;
+  const questionInput1 = document.querySelector("input[name='question1']:checked").value;
+  const questionInput2 = document.querySelector("input[name='question2']:checked").value;
+  const questionInput3 = document.querySelector("input[name='question3']:checked").value;
+  const questionInput4 = document.querySelector("input[name='question4']:checked").value;
+  const questionInput5 = document.querySelector("input[name='question5']:checked").value;
   
   let score;
   if (questionInput1 === "yes" || questionInput3 === "yes") {
@@ -25,14 +25,14 @@ function handleSurvey(event) {
   } else {
     result = "HTML";
   }
-  document.querySelector("span#output").innerText = result;
-  suggesion.removeAttribute("class");    
+  document.querySelector("span#output").innerText = result;    
 }
 
 window.addEventListener("load", function() {
   let form = document.querySelector("form#survey");
   let suggestion = document.querySelector("div#suggestion");
-  form.addEventListener("submit", handleSurvey);
+  form.addEventListener("submit", handleSuggestion);
+  suggestion.removeAttribute("class");
 });
 
 
