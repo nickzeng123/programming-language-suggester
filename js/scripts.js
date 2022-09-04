56dd7083cf05e792be8a748c0c06114ce31a43b6
Nicholas Zeng <nickyxzeng@hotmail.com>
2022-09-04 01:51:50 -0700

    fix reset function in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function handleSuggestion(event) {
   event.preventDefault();
-  const questionInput1 = document.querySelector("input[name='question1Input']:checked").value;
-  const questionInput2 = document.querySelector("input[name='question2Input']:checked").value;
-  const questionInput3 = document.querySelector("input[name='question3Input']:checked").value;
-  const questionInput4 = document.querySelector("input[name='question4Input']:checked").value;
-  const questionInput5 = document.querySelector("input[name='question5Input']:checked").value;
+  let questionInput1 = document.querySelector("input[name='question1Input']:checked").value;
+  let questionInput2 = document.querySelector("input[name='question2Input']:checked").value;
+  let questionInput3 = document.querySelector("input[name='question3Input']:checked").value;
+  let questionInput4 = document.querySelector("input[name='question4Input']:checked").value;
+  let questionInput5 = document.querySelector("input[name='question5Input']:checked").value;
   
   let score;
   if (questionInput1 === "yes" || questionInput3 === "yes") {
@@ -19,34 +19,38 @@ function handleSuggestion(event) {
   if (score === 1) {
     result = "Python";
   } else if (score === 2) {
-    result = "Javascript";
+    result = "JavaScript";
   } else if (score === 3) {
     result = "Swift";
   } else {
     result = "HTML";
   }
   document.querySelector("span#output").innerText = result;   
-   suggestion.removeAttribute("class");
+  suggestion.removeAttribute("class");
 }
-
-form.addEventListener("submit", function() {
-  resetBtn.removeAttribute("class");
-});
 
 window.addEventListener("load", function() {
   let form = document.querySelector("form#survey");
   let suggestion = document.querySelector("div#suggestion");
   let resetBtn = document.querySelector("button#reset");
+
   form.addEventListener("submit", handleSuggestion);
+
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function() {
+    suggestion.setAttribute("class", "hidden");
+    document.getElementById("question1Input").value = null;
+    document.getElementById("question2Input").value = null;
+    document.getElementById("question3Input").value = null;
+    document.getElementById("question4Input").value = null;
+    document.getElementById("question5Input").value = null;
+  })
 });
 
-resetBtn.addEventListener("click", function() {
-  document.querySelector("questionInput1").value = null;
-  document.querySelector("questionInput2").value = null;
-  document.querySelector("questionInput3").value = null;
-  document.querySelector("questionInput4").value = null;
-  document.querySelector("questionInput5").value = null;
-})
+
 
 
 
